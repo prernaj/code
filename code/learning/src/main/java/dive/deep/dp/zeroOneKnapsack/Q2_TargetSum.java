ccd7866b2675e944ebca7abbd26892d03cc7ee73
Prerna Jeslani <prernajeslani@Prernas-MacBook-Pro.local>
2021-11-28 20:31:11 -0800

    dp

--- code/learning/src/main/java/dive/deep/dp/zeroOneKnapsack/Q2_TargetSum.java
+++ code/learning/src/main/java/dive/deep/dp/zeroOneKnapsack/Q2_TargetSum.java
@@ -1,5 +1,25 @@
 package dive.deep.dp.zeroOneKnapsack;
 
+/**
+ * Question
+ * You are given an integer array nums and an integer target.
+ * You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+ * Return the number of different expressions that you can build, which evaluates to target.
+ */
+
+/**
+ * nums = [1,1,1,1,1] and target = 3 => 5
+ * nums = [1] and target = 1 => 1
+ */
+
+ /**
+  * Thought process
+  * Brute force.
+  * try to put + and - on every element and filter out assgnments which leads to required result S
+  * 
+  * 
+  */
+
 public class Q2_TargetSum {
     
 }
